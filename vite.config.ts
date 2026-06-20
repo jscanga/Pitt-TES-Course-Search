959d691dec7f977f65f29e0c62b59642b06a58a6
James Scanga <jascanga2@gmail.com>
2026-06-19 21:00:39 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: { target: 'es2022' },
+  optimizeDeps: { esbuildOptions: { target: 'es2022' } },
 });
